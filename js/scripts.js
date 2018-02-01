0f84ea77a137befd47b232acb6e4281bb69ae916
Daniel Turlinski <daniel.turlinski@gmail.com>
2018-02-01 03:31:45 +0100

    Fix CORS problem

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 // scripts.js
 
 $(function() {
-
 	var url = 'http://api.icndb.com/jokes/random';
 		$button = $('#get-joke').click(function() {
 			getJoke();
@@ -19,11 +18,13 @@ $(function() {
 		});
 	};
 
-	var tweetLink = "https://twitter.com/intent/tweet?text=";
+	var prefix = "https://cors-anywhere.herokuapp.com/";
+		tweetLink = "https://twitter.com/intent/tweet?text=";
 		quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 	function getQuote() {
-		$.getJSON(quoteUrl, createTweet);
+		$.getJSON(prefix + quoteUrl, createTweet);
+		$.ajaxSetup({ cache: false });
 	};
 
 	function createTweet(input) {
@@ -35,7 +36,7 @@ $(function() {
 			quoteAuthor = 'Unknown author';
 		};
 
-		var tweetText = 'Quote of the day - ' + quoteText + 'Author:' + QuoteAuthor;
+		var tweetText = 'Quote of the day - ' + quoteText + 'Author:' + quoteAuthor;
 
 		if (tweetText.length > 140) {
 			getQuote();
